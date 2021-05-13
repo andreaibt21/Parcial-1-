b3865d6cced3a00a07314d748eefc85658704b78
andreaibt21 <andreaixabella@gmail.com>
2021-05-13 18:46:28 -0300

    Creando repositorio

---
+++ src/utn.c
@@ -0,0 +1,12 @@
+/*
+ * utn.c
+ *
+ *  Created on: May 13, 2021
+ *      Author: andrea briceño
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
---
+++ src/utn.h
@@ -0,0 +1,13 @@
+/*
+ * utn.h
+ *
+ *  Created on: May 13, 2021
+ *      Author: andrea briceño
+ */
+
+#ifndef UTN_H_
+#define UTN_H_
+
+
+
+#endif /* UTN_H_ */
